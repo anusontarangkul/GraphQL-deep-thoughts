06b2ca6ed2c403c07708996ee617d6b6cbcef8fc
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-20 13:09:13 -0700

    set up schemas

--- server/server.js
+++ server/server.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const db = require('./config/connection');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  })
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+  await server.start()
+
+
+  server.applyMiddleware({ app });
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    });
   });
-});
+}
+
+startApolloServer()
+
+
