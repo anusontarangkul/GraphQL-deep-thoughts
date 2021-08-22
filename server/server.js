b254c3ab66065a3d4d5c9429ae1e838657a0105b
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-08-21 17:27:09 -0700

    configure root package

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas')
-const { authMiddleware } = require('./utils/auth')
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -21,6 +22,14 @@ async function startApolloServer() {
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  })
 
   db.once('open', () => {
     app.listen(PORT, () => {
